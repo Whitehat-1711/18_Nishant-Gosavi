5d9cd997e62ddd4d748773df742daeb9d26df899
pushpendar881 <149162648+pushpendar881@users.noreply.github.com>
2024-03-23 18:35:56 +0530

    Update AI content Authenticity challenge.py

--- AI content Authenticity challenge.py
+++ AI content Authenticity challenge.py
@@ -1,6 +1,31 @@
 import pandas as pd
 import numpy as np
 import seaborn as sns
+
 dataset=pd.read_csv('AI_Human.csv.zip')
+
 dataset.head()
+
 dataset.info()
+
+dataset.describe()
+
+sns.countplot(data=dataset,x='generated')
+
+print('total text: ',dataset['generated'].count())
+print('Human Written Texts: ',(dataset['generated']==0.0).sum())
+print('AI Generated Texts: ',(dataset['generated']==1.0).sum())
+
+dataset['text'][0]
+
+def remove_tags(text):
+    tags=['\n','\'']
+    for tag in tags:
+        text=text.replace(tag,'')
+        
+    return text
+dataset['text']=dataset['text'].apply(remove_tags)
+
+dataset['text'][0]
+
+
