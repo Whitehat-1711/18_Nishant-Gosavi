bac22d74bcca4fc5a393323c08140f47c40a020c
Whitehat-1711 <97377429+Whitehat-1711@users.noreply.github.com>
2024-03-23 20:03:46 +0530

    Update AI content Authenticity challenge.py

--- AI content Authenticity challenge.py
+++ AI content Authenticity challenge.py
@@ -5,7 +5,12 @@ import seaborn as sns
 dataset=pd.read_csv('AI_Human.csv.zip')
 
 dataset.head()
+dataset.info()
+dataset.describe()
+sns.countplot(data=dataset,x='generated')
 
-
+print('total text: ',dataset['generated'].count())
+print('Human Written Texts: ',(dataset['generated']==0.0).sum())
+print('AI Generated Texts: ',(dataset['generated']==1.0).sum())
 
 
